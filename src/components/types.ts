8c57c53900479c8aeda642a2ae29171c2f05a1bf
Andrew Leykin <andrewleyykin@mail.ru>
2022-08-29 14:29:56 +0300

    feat: add export default

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,3 +1,5 @@
+import Vue from "vue";
+
 export type TDevtoolsItem<T = string> = {
   key: T;
   saveLocal?: boolean;
@@ -7,3 +9,7 @@ export type TChangePayload<T = string> = {
   key: T;
   value: boolean;
 };
+
+declare class KeDevtools extends Vue {}
+
+export default KeDevtools;
